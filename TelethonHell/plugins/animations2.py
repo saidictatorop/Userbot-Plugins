59498f917e397d32ea65059f0d8bad4b624aec73
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-08-13 06:40:18 +0530

    Update animations2.py

--- TelethonHell/plugins/animations2.py
+++ TelethonHell/plugins/animations2.py
@@ -153,7 +153,7 @@ async def _(event):
         idd = reply_message.sender_id
         if idd == 609517172:
             await event.edit(
-                "This is My Master\n**How dare you trying to tell me to kill master nigger!**\n\n__Your account is on hold! Pay 99$ to my master__ [HellBoy](https://t.me/MissRadha) __to release your account__😏"
+                "This is My Master\n**How dare you trying to tell me to kill master nigger!**\n\n__Your account is on hold! Pay 99$ to my master__ [Radha](https://t.me/MissRadha) __to release your account__😏"
             )
         else:
             await event.edit("`K i l l i n g . . . . . . .`")
